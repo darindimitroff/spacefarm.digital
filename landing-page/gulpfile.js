1c0d42b802505fbeb5b568fcc4e68823daecfab6
Darin Dimitroff <d@thecrazyones.agency>
2016-10-24 22:35:49 +0300

    WIP

--- landing-page/gulpfile.js
+++ landing-page/gulpfile.js
@@ -6,7 +6,6 @@ var browserSyncLib = require('browser-sync');
 var pjson = require('./package.json');
 var minimist = require('minimist');
 var wrench = require('wrench');
-
 // Load all gulp plugins based on their names
 // EX: gulp-copy -> copy
 var plugins = gulpLoadPlugins();
